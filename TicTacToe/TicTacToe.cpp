60192caecabf458b7326f0650978b07709228206
Zorikto Baldanov <45837027+Paarkzb@users.noreply.github.com>
2021-08-03 22:50:02 +0800

    Update TicTacToe.cpp

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -1,20 +1,25 @@
 ﻿// TicTacToe.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
-#include <iostream>
+#include <stdio.h>
+#include <Windows.h>
 
-int main()
+VOID ErrorExit(LPCSTR);
+
+int main(VOID)
 {
-    std::cout << "Hello World!\n";
+    int const nScreenWidth = 80;
+    int const nScreenHeigth = 30;
+
+    wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeigth];
+    for (int i = 0; i < nScreenHeigth * nScreenHeigth; i++)
+        screen[i] = L' ';
+
+    HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+    SetConsoleActiveScreenBuffer(hConsole);
+    DWORD dwBytesWritten = 0;
+
+    CloseHandle(hConsole);
+    system("pause");
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
